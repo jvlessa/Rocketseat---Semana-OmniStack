9a0de4674a62f5ab5f384ec233c22d9376f0b1ce
João Vitor Lessa <joaovitorlessa@gmail.com>
2019-08-07 23:06:50 -0300

    Utilizando controller Dev simples

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,14 +1,20 @@
 const express = require('express');
+const DevController = require('./controllers/DevController');
 
 const routes = express.Router();
 
+// Root Get
 routes.get('/', (req, res) => {
     return res.json( {message: `Hello ${req.query.name}`} );
 });
 
-routes.post('/devs', (req, res) => {
+// Post para testar
+routes.post('/postPrimario', (req, res) => {
     console.log(req.body);
     return res.json( {ok: true} );
 });
+
+// Rota com controller
+routes.post('/devs', DevController.store);
 
 module.exports = routes;
