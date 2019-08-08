e3bd5e355a941e30f27ed3bed21a85408d6fc9ad
João Vitor Lessa <joaovitorlessa@gmail.com>
2019-08-07 23:17:30 -0300

    Dev Controller com API do Github

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,5 +1,13 @@
+const axios = require('axios');
+
 module.exports = {
-    store (req, res){
-        return res.json({ ok: true });
+    // Assíncronidade caso use o Await (pois a requisição demora um pouquinho)
+    async store (req, res){
+        //console.log(req.body.username);
+        const { username } = req.body;
+        
+        //Acessando a API do Github utilizando o Axios
+        const response = await axios.get(`https://api.github.com/users/${username}`);
+        return res.json(response.data);
     }
 };
