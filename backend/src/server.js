bab6f986324f9389fe1df6f974ff0e6555006699
João Vitor Lessa <joaovitorlessa@gmail.com>
2019-08-07 22:56:18 -0300

    Conectando Mongoose

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,21 @@
 //Importando Express
 const express = require('express');
+const mongoose = require('mongoose');
+
 const routes = require('./routes');
 
 //Criando server baseado na instância do express
 const server = express();
+
+//Conexão com o banco de dados feita
+mongoose.connect('mongodb+srv://joaolessab:minhasenha@cluster0-znguo.mongodb.net/omnistack8?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+});
 
 server.use(express.json());
 server.use(routes);
 
 //Posso escolher qual porta usar
 server.listen(3333);
+
+//M - Model, V - View, C - Controller
