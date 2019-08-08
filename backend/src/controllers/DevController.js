afb4501a98e3ef42b4f12fc770a918b656cac823
João Vitor Lessa <joaovitorlessa@gmail.com>
2019-08-07 23:24:55 -0300

    Consumindo API do Github e criando usuário no banco

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,13 +1,27 @@
 const axios = require('axios');
+const Dev = require('../models/Dev');
 
 module.exports = {
     // Assíncronidade caso use o Await (pois a requisição demora um pouquinho)
     async store (req, res){
         //console.log(req.body.username);
         const { username } = req.body;
-        
+   
         //Acessando a API do Github utilizando o Axios
         const response = await axios.get(`https://api.github.com/users/${username}`);
-        return res.json(response.data);
+        
+        //Lendo data da request do Github (renomeando campo avatar de retorno)
+        const { name, bio, avatar_url: avatar } = response.data;
+
+        //Armazenando no banco de dados utilizando o model e linkando os campos
+        const dev = await Dev.create({ 
+            name: name, 
+            user: username,
+            bio: bio,
+            avatar: avatar
+         });
+        
+        //Retornando a varíavel dev de criação de novo usuário no banco
+        return res.json(dev);
     }
 };
