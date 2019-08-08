519e2f45aacbefa6f19a7d0ceb294ac0f573a58e
João Vitor Lessa <joaovitorlessa@gmail.com>
2019-08-08 00:04:28 -0300

    Dislikes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const DevController = require('./controllers/DevController');
 const LikeController = require('./controllers/LikeController');
+const DislikeController = require('./controllers/DislikeController');
 
 const routes = express.Router();
 
@@ -18,5 +19,6 @@ routes.post('/postPrimario', (req, res) => {
 // Rota com controller
 routes.post('/devs', DevController.store);
 routes.post('/devs/:devId/likes', LikeController.store);
+routes.post('/devs/:devId/dislikes', DislikeController.store);
 
 module.exports = routes;
