7e7d2546c8514fef615d9ea0408dd9dc3d653b49
João Vitor Lessa <joaovitorlessa@gmail.com>
2019-08-19 21:15:08 -0300

    Limpeza da interface web - 1

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,4 +4,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+// Nessa linha é possível observar que estamos pegando o arquivo "App.js" e usando-o para renderizar a div "root"
+//Componente global
 ReactDOM.render(<App />, document.getElementById('root'));
