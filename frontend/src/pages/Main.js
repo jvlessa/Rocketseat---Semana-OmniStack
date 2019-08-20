654659e5e5dc67f5ce7331e21688a1c9819de61b
João Vitor Lessa <joaovitorlessa@gmail.com>
2019-08-20 00:39:50 -0300

    Like e link to

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import './Main.css';
 
@@ -29,7 +30,14 @@ export default function Main({ match }){
     }, [ match.params.id ])
 
     async function handleLike(id){
-        console.log('like', id);
+        await api.post(`/devs/${id}/likes`, null, {
+            headers: {
+                user: match.params.id
+            },
+        })
+
+        //Fazendo usuario nao aparecer mais logo apos o evento (temos que usar o setUsers, sobscrevendo a variavel)
+        setUsers(users.filter(user => user._id !== id));
     }
 
     async function handleDislike(id){
@@ -40,33 +48,39 @@ export default function Main({ match }){
         })
 
         //Fazendo usuario nao aparecer mais logo apos o evento (temos que usar o setUsers, sobscrevendo a variavel)
-        setUsers(users.filter(user => user._id != id));
+        setUsers(users.filter(user => user._id !== id));
     }
 
     return (
         <div className="main-container">
-            <img src={ logo } alt="Tindev" className="logoImage" />
-            <ul> 
-                {users.map(user => (
-                    <li key={user._id}>
-                        <img src={user.avatar} alt={user.name} />
-                        <footer>
-                            <strong>{user.name}</strong>
-                            <p>{user.bio}</p>
-                        </footer>
-
-                        <div className="buttons">
-                            {/*listener para disparar apenas quando o usuario clicar: "() =>" - caso contrário, ele irá executar no momento da renderização*/}
-                            <button type="button" onClick={() => handleDislike(user._id)}>
-                                <img src={dislike} alt="Dislike"/>  
-                            </button>
-                            <button type="button" onClick={() => handleLike(user._id)}>
-                                <img src={like} alt="Like"/>
-                            </button>
-                        </div>
-                    </li>    
-                ))}
-            </ul>
+            <Link to="/">
+                <img src={ logo } alt="Tindev" className="logoImage" />       
+            </Link>     
+                { users.length > 0 ? (
+                    <ul>
+                        {users.map(user => (
+                                <li key={user._id}>
+                                    <img src={user.avatar} alt={user.name} />
+                                    <footer>
+                                        <strong>{user.name}</strong>
+                                        <p>{user.bio}</p>
+                                    </footer>
+            
+                                    <div className="buttons">
+                                        {/*listener para disparar apenas quando o usuario clicar: "() =>" - caso contrário, ele irá executar no momento da renderização*/}
+                                        <button type="button" onClick={() => handleDislike(user._id)}>
+                                            <img src={dislike} alt="Dislike"/>  
+                                        </button>
+                                        <button type="button" onClick={() => handleLike(user._id)}>
+                                            <img src={like} alt="Like"/>
+                                        </button>
+                                    </div>
+                                </li>   
+                        ))}
+                    </ul>
+                ) : (
+                    <div className="empty">Acabou</div>
+                ) }
         </div>
         //<h1>{ match.params.id }</h1>
     )
