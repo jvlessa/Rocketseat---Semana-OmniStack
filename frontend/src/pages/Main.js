0593dbe1961cf21b5a814ef58a86a0144e9cf9e5
João Vitor Lessa <joaovitorlessa@gmail.com>
2019-08-20 00:01:25 -0300

    Tela inicial de usuarios - WEB

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,6 +1,70 @@
 import React from 'react';
 
+import './Main.css';
+
+import logo from '../assets/logo.svg';
+import like from '../assets/like.svg';
+import dislike from '../assets/dislike.svg';
+
 export default function Main({ match }){
-    //Acessando ID do usuário vindo do routes.js
-    return <h1>{ match.params.id }</h1>;
+    return (
+        <div className="main-container">
+            <img src={ logo } alt="Tindev" className="logoImage" />
+            <ul>
+                
+                <li>
+                    <img src="https://avatars2.githubusercontent.com/u/21973502?s=460&v=4" alt="" />
+                    <footer>
+                        <strong>João Vitor</strong>
+                        <p>Testando tudo, 21</p>
+                    </footer>
+
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={dislike} alt="Dislike"/>  
+                        </button>
+                        <button type="button">
+                            <img src={like} alt="Like"/>
+                        </button>
+                    </div>
+                </li>
+
+                <li>
+                    <img src="https://avatars2.githubusercontent.com/u/21973502?s=460&v=4" alt="" />
+                    <footer>
+                        <strong>João Vitor</strong>
+                        <p>Testando tudo, 21</p>
+                    </footer>
+
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={dislike} alt="Dislike"/>  
+                        </button>
+                        <button type="button">
+                            <img src={like} alt="Like"/>
+                        </button>
+                    </div>
+                </li>
+
+                <li>
+                    <img src="https://avatars2.githubusercontent.com/u/21973502?s=460&v=4" alt="" />
+                    <footer>
+                        <strong>João Vitor</strong>
+                        <p>Testando tudo, 21</p>
+                    </footer>
+
+                    <div className="buttons">
+                        <button type="button">
+                            <img src={dislike} alt="Dislike"/>  
+                        </button>
+                        <button type="button">
+                            <img src={like} alt="Like"/>
+                        </button>
+                    </div>
+                </li>
+
+            </ul>
+        </div>
+        //<h1>{ match.params.id }</h1>
+    )
 }
